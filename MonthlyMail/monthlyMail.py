9496b3a55fb01de483af76d6b8f2edc2ef37abfe
Fran <kidstypike780@gmail.com>
2017-01-22 11:18:56 +0100

    Update and rename monthlymail.py to monthlyMail.py

--- MonthlyMail/monthlyMail.py
+++ MonthlyMail/monthlyMail.py
@@ -1,4 +1,4 @@
-import smtplib, datetime, shelve, sys, anydbm
+import smtplib, datetime, shelve, sys, anydbm, sendMail
 anydbm._defaultmod = __import__('dumbdbm')
 #anydb part added bc otherwise shelve causes an error when loaded via LaunchControl, dunno why
 
@@ -15,14 +15,14 @@ if lastMonth == thisMonth:
 else:
     subject = ""      # CUSTOMIZE THESE VARIABLES
     message = ""      #  
-    signoff = ""      #
+    signoff = ""      # (You can leave this empty if you don't need to add to your message (see below))
 
-# in my case, I need to add a line to my message every three months, uncomment if you do too
-#    if thisMonth == (10 or 1 or 4 or 7):
-#        message += ""
-
-    sendMail()
+# in my case, I need to add a line to my message every three months, uncomment and customize if you do too
+#    if thisMonth == (10 or 1 or 4 or 7):       # customize months with different message
+#        message += ""                          # add line(s)
+    body = message + signoff
     
+    sendMail(subject, body):
     shelfFile['lastMonth'] = thisMonth
     shelfFile.close()
 
