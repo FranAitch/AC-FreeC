92974f3eef3cac4593a7f1ab876d04fc138c0b30
Fran <kidstypike780@gmail.com>
2017-01-24 19:47:53 +0100

    Added "in-script scheduler"

--- AC-FreeC/acfreec.py
+++ AC-FreeC/acfreec.py
@@ -1,26 +1,31 @@
 # AC/FreeC prototype
-import json, requests, textmyself
+import json, requests, textmyself, time
 
 # Set this to your location id, e.g. "2657360"
 location_id = ""
-
 # Set this to your app id (long string of numbers and letters)
 app_id = ""
-
 # Set this to the indoor temperature you don't want to exceed
-indoorTemp = 20.0
+indoorTemp = 23.0
+# Set this to an integer like 30 (checks every half hour) or 60 (checks every hour)
+# If you use some type of scheduler, leave this at None
+checkInterval = None
 
+def checkWeather():
+    url = 'http://api.openweathermap.org/data/2.5/weather?id={}&APPID={}&units=metric' .format(location_id, app_id)
+    response = requests.get(url)
+    response.raise_for_status
+    weatherData = json.loads(response.text)
 
-url = 'http://api.openweathermap.org/data/2.5/weather?id={}&APPID={}&units=metric' .format(location_id, app_id)
-response = requests.get(url)
-response.raise_for_status
-weatherData = json.loads(response.text)
+    weatherGroup = weatherData['weather'][0]['id']
+    mainTemp = weatherData['main']['temp']
+    
+    message = getText(mainTemp, weatherGroup)
+    if message != None:
+        textmyself.textmyself(message)
+    
 
-weatherGroup = weatherData['weather'][0]['id']
-mainTemp = weatherData['main']['temp']
-
-
-def getText():
+def getText(mainTemp, weatherGroup):
     if mainTemp >= (indoorTemp - 1):
         text = "It's {}°C outside. Time to close the windows." .format(mainTemp)
     elif str(weatherGroup).startswith('3'):
@@ -32,7 +37,11 @@ def getText():
     return text
 
 
-message = getText()
-if message != None:
-    textmyself.textmyself(message)
-
+def main():
+    while True:
+        checkWeather()
+        if checkInterval:
+            for i in range(checkInterval*60):
+                time.sleep(1)
+        else:
+            break
